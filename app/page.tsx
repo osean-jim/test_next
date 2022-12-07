a915680c782b889def98a50e1f20f1c3bc65dd5c
jungjinsoo <jim@o-sean.io>
2022-12-07 15:44:32 +0900

    feat : tailwindcss 및 next.js 13 새로이 적용
    - 새로운 명령어로 적용
    - tailwindcss 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,63 @@
+"use client";
+import { useEffect, useState } from "react";
+import detectEthereumProvider from "@metamask/detect-provider";
+
 export default function Home() {
+  const [ isMetaMaskInstalled, setIsMetaMaskInstalled ] = useState(false);
+  const [ isMetaMaskConnected, setIsMetaMaskConnected ] = useState(false);
+
+
+  const handleMetaMaskDisconnect = async () => {
+    const isConnected = window.ethereum.isConnected();
+    console.log(isConnected);
+    if (isConnected) {
+      await window.ethereum.request({
+        method: "wallet_requestPermissions",
+        params: [
+          {
+            eth_accounts: {}
+          }
+        ]
+      });
+    }
+  }
+  const handleMetaMaskConnect = () => {
+    if (!isMetaMaskInstalled) {
+      alert('메타마스크를 설치해야 합니다.');
+    } else {
+      window.ethereum.request({ method: 'eth_requestAccounts' }).then((accounts: any) => {
+        // accounts[0] is the address of the currently selected account
+        console.log(accounts[0], 'result data');
+      }).catch((err: any) => {
+        alert(err.message)
+      })
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      const provider = await detectEthereumProvider();
+      if (provider) {
+        // From now on, this should always be true:
+        // provider === window.ethereum
+        setIsMetaMaskInstalled(true);
+      } else {
+        console.log("Please install MetaMask!");
+        setIsMetaMaskInstalled(false);
+      }
+    })();
+  }, []);
+
   return (
-    <h1 className={'text-3xl font-bold underline'}>
-      Hello World
-    </h1>
-  )
+    <>
+      <div style={{ padding: 30 }}>
+        <h1 className={"text-3xl font-bold underline"}>
+          Hello World
+        </h1>
+        <button className={"btn btn-active"} onClick={handleMetaMaskConnect}>메타마스크 연결</button>
+        <br />
+        <button className={"btn btn-active"} onClick={handleMetaMaskDisconnect}>메타마스크 비연결</button>
+      </div>
+    </>
+  );
 };
